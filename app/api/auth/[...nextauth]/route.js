905870f19261a580756f149ccec34771e4c38a00
Code-Algo <alexanderacollins@gmail.com>
2023-11-03 23:52:05 -0700

    Connecting to Mongo

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+import { connectToDB } from '@utils/database';
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({
@@ -13,8 +15,16 @@ const handler = NextAuth({
     },
     async signIn({ profile }) {
         try{
-            // serverless -> Lambda -> dynamodb
+            await connectToDB();
+
+            // check if a user already exists
+
+            // if not create a new user
+
+            return true;
         } catch (error) {
+            console.log(error);
+            return false;
 
 
         }
